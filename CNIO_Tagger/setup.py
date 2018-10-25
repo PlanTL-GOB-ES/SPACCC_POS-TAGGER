fe917da866aeadf3e788ed0f39dd7b5b233d6e03
Felipe Soares <soarescmsa@gmail.com>
2018-10-25 09:52:50 +0200

    Fixed python setup bug

--- CNIO_Tagger/setup.py
+++ CNIO_Tagger/setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 setup(
     name='CNIO_Tagger',
     version='0.1',
-    description='Python Wrapper for the CNIO POS Tagger'
-    author='Felipe Soares - felipe.soares@bsc.es
+    description='Python Wrapper for the CNIO POS Tagger',
+    author='Felipe Soares - felipe.soares@bsc.es',
     url='https://github.com/PlanTL/SPACCC_POS-TAGGER/CNIO_Tagger',
     py_modules=['CNIO_Tagger']
 )
